0a6922595f6b9cbdcf4ef943c4220405311fd244
breno-do-val <brenorvale@gmail.com>
2020-08-26 00:49:23 -0300

    OnTriggerEnter for creating new apples as the game is running

---
+++ Assets/Scripts/ControladorJogo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ControladorJogo : MonoBehaviour
+{
+
+    [Tooltip("Referência para a maça")]
+    public Transform apple;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Apple"))
+        {
+            Debug.Log("Touch it");
+            //Instantiate(apple, new Vector3(1.0f, 3.0f, 0.0f), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
