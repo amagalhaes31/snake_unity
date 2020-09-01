db399b20248c26343895f6171501d5131df4230f
amagalhaes31 <alexandremagalhaes31@gmail.com>
2020-08-31 22:15:53 -0300

    Add audio.

---
+++ Assets/Scripts/MusicaControle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicaControle : MonoBehaviour
+{
+
+    public static MusicaControle musicaControle = null;
+
+    private void Awake()        //Singleton
+    {
+        if (musicaControle != null) {
+            Destroy(gameObject);
+        } 
+        else {
+            musicaControle = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //A Game Object não destruida pela Unity quando mudar de cena
+        GameObject.DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
