9638512613cc56f5d32769817422c66c95f5df39
amagalhaes31 <alexandremagalhaes31@gmail.com>
2020-08-31 21:36:33 -0300

    Add the Initial Scene.

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuPrincipal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Metodo para carregar uma scene
+    /// </summary>
+    /// <param name="nomeScene">Nome da scene que sera carregada</param>
+    public void CarregaScene(string nomeScene)
+    { 
+        SceneManager.LoadScene(nomeScene);        
+    }
+}
